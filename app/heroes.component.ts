6a8b5b9858381ada65aba70ff4f902fa0d65c445
knikolaenko <kir.nikolaenko@gmail.com>
2016-10-06 17:34:28 +0300

    7. Animations

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import {
+    Component, OnInit, trigger,
+    state,
+    style,
+    animate,
+    transition,
+    keyframes
+} from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 import {Router} from "@angular/router";
 @Component({
     moduleId: module.id,
     selector: 'my-heroes',
     templateUrl: 'heroes.component.html',
-    styleUrls: [ 'heroes.component.css' ]
+    styleUrls: ['heroes.component.css'],
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({transform: 'translateX(0)'})),
+            transition('void => *', [
+                animate(300, keyframes([
+                    style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+                    style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+                    style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+                ]))
+            ]),
+            transition('* => void', [
+                animate(300, keyframes([
+                    style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+                    style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+                    style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+                ]))
+            ])
+        ])
+    ]
 })
 export class HeroesComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
 
-    constructor(
-        private router: Router,
-        private heroService: HeroService) { }
+    constructor(private router: Router,
+                private heroService: HeroService) {
+    }
 
     getHeroes(): void {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -34,7 +60,9 @@ export class HeroesComponent implements OnInit {
 
     add(name: string): void {
         name = name.trim();
-        if (!name) { return; }
+        if (!name) {
+            return;
+        }
         this.heroService.create(name)
             .then(hero => {
                 this.heroes.push(hero);
@@ -47,7 +75,9 @@ export class HeroesComponent implements OnInit {
             .delete(hero.id)
             .then(() => {
                 this.heroes = this.heroes.filter(h => h !== hero);
-                if (this.selectedHero === hero) { this.selectedHero = null; }
+                if (this.selectedHero === hero) {
+                    this.selectedHero = null;
+                }
             });
     }
 }
